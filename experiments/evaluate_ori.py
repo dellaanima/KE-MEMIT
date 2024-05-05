ae90fe5aa2ee372d1ab5325cf807084cf1541a41
dellaanima <dellaanima@sookmyung.ac.kr>
2024-05-05 07:42:57 +0000

    bash_files

---
+++ experiments/evaluate_ori.py
@@ -0,0 +1,385 @@
+import json
+import shutil
+from itertools import islice
+from time import time
+from typing import Tuple, Union
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from baselines.ft import FTHyperParams, apply_ft_to_model
+from baselines.mend import MENDHyperParams, MendRewriteExecutor
+from dsets import (
+    AttributeSnippets,
+    CounterFactDataset,
+    MENDQADataset,
+    MultiCounterFactDataset,
+    get_tfidf_vectorizer,
+)
+from experiments.py.eval_utils_counterfact import compute_rewrite_quality_counterfact
+from experiments.py.eval_utils_zsre import compute_rewrite_quality_zsre
+from memit import MEMITHyperParams, apply_memit_to_model
+from rome import ROMEHyperParams, apply_rome_to_model
+from util import nethook
+from util.globals import *
+from experiments.causal_trace import make_inputs
+
+
+ALG_DICT = {
+    "MEMIT": (MEMITHyperParams, apply_memit_to_model),
+    "ROME": (ROMEHyperParams, apply_rome_to_model),
+    "FT": (FTHyperParams, apply_ft_to_model),
+    "MEND": (MENDHyperParams, MendRewriteExecutor().apply_to_model),
+}
+
+DS_DICT = {
+    "mcf": (MultiCounterFactDataset, compute_rewrite_quality_counterfact),
+    "cf": (CounterFactDataset, compute_rewrite_quality_counterfact),
+    "zsre": (MENDQADataset, compute_rewrite_quality_zsre),
+}
+
+
+
+# added for filtering the requests 
+def predict_token(model, tokenizer, prompts, return_p=False):
+    inp = make_inputs(tokenizer, prompts)
+    preds, p = predict_from_input(model, inp)
+    result = [tokenizer.decode(c) for c in preds]
+    if return_p:
+        result = (result, p)
+    return result
+
+def predict_from_input(model, inp):
+    out = model(**inp)["logits"]
+    probs = torch.softmax(out[:, -1], dim=1)
+    p, preds = torch.max(probs, dim=1)
+    return preds, p
+
+# Utilities for dealing with tokens
+def make_inputs(tokenizer, prompts, device="cuda"):
+    token_lists = [tokenizer.encode(p) for p in prompts]
+    maxlen = max(len(t) for t in token_lists)
+    if "[PAD]" in tokenizer.all_special_tokens:
+        pad_id = tokenizer.all_special_ids[tokenizer.all_special_tokens.index("[PAD]")]
+    else:
+        pad_id = 0
+    input_ids = [[pad_id] * (maxlen - len(t)) + t for t in token_lists]
+    # position_ids = [[0] * (maxlen - len(t)) + list(range(len(t))) for t in token_lists]
+    attention_mask = [[0] * (maxlen - len(t)) + [1] * len(t) for t in token_lists]
+    return dict(
+        input_ids=torch.tensor(input_ids).to(device),
+        #    position_ids=torch.tensor(position_ids).to(device),
+        attention_mask=torch.tensor(attention_mask).to(device),
+    )
+
+def decode_tokens(tokenizer, token_array):
+    if hasattr(token_array, "shape") and len(token_array.shape) > 1:
+        return [decode_tokens(tokenizer, row) for row in token_array]
+    return [tokenizer.decode([t]) for t in token_array]
+
+def filter_requests(model, tokenizer, requests):
+    filtered_requests = []
+    for request in requests:
+        prompt = request['prompt'].format(request['subject'])
+        inp = make_inputs(tokenizer, [prompt])
+        
+        # Generate tokens
+        preds, _ = predict_from_input(model, inp)
+        generated_text = decode_tokens(tokenizer, preds)[0]
+        # Filter requests based on generated text matching target_true
+        if generated_text.strip().lower() == request['target_true']['str'].lower():
+            filtered_requests.append(request)
+    
+    return filtered_requests
+
+
+
+def main(
+    alg_name: str,
+    model_name: Union[str, Tuple],
+    hparams_fname: str,
+    ds_name: str,
+    dataset_size_limit: int,
+    continue_from_run: str,
+    skip_generation_tests: bool,
+    generation_test_interval: int,
+    conserve_memory: bool,
+    dir_name: str,
+    num_edits: int = 1,
+    use_cache: bool = False,
+):
+    
+    # Set algorithm-specific variables
+    params_class, apply_algo = ALG_DICT[alg_name]
+
+    
+    
+
+    # Determine run directory
+    # Create new dir if not continuing from prev run OR prev run doesn't exist
+    if (
+        continue_from_run is None
+        or not (run_dir := RESULTS_DIR / dir_name / continue_from_run).exists()
+    ):
+        continue_from_run = None
+    if continue_from_run is None:
+        alg_dir = RESULTS_DIR / dir_name
+        if alg_dir.exists():
+            id_list = [
+                int(str(x).split("_")[-1])
+                for x in alg_dir.iterdir()
+                if str(x).split("_")[-1].isnumeric()
+            ]
+            run_id = 0 if not id_list else max(id_list) + 1
+        else:
+            run_id = 0
+        run_dir = RESULTS_DIR / dir_name / f"run_{str(run_id).zfill(3)}"
+        
+        run_dir.mkdir(parents=True, exist_ok=True)
+    print(f"Results will be stored at {run_dir}")
+
+    # Get run hyperparameters
+    params_path = (
+        run_dir / "params.json"
+        if continue_from_run is not None
+        else HPARAMS_DIR / alg_name / hparams_fname
+    )
+    hparams = params_class.from_json(params_path)
+    if not (run_dir / "params.json").exists():
+        shutil.copyfile(params_path, run_dir / "params.json")
+    print(f"Executing {alg_name} with parameters {hparams}")
+
+    # Instantiate vanilla model
+    if type(model_name) is str:
+        print("Instantiating model")
+        model = AutoModelForCausalLM.from_pretrained(model_name).cuda()
+        tok = AutoTokenizer.from_pretrained(model_name)
+        tok.pad_token = tok.eos_token
+    else:
+        model, tok = model_name
+        model_name = model.config._name_or_path
+
+    # Load data
+    print("Loading dataset, attribute snippets, tf-idf data")
+    snips = AttributeSnippets(DATA_DIR) if not skip_generation_tests else None
+    vec = get_tfidf_vectorizer(DATA_DIR) if not skip_generation_tests else None
+
+    if num_edits > 1:
+        assert ds_name != "cf", f"{ds_name} does not support multiple edits"
+
+    ds_class, ds_eval_method = DS_DICT[ds_name]
+    ds = ds_class(DATA_DIR, tok=tok, size=dataset_size_limit)
+
+    # Get cache templates
+    cache_template = None
+    if use_cache:
+        cache_template = (
+            KV_DIR
+            / f"{model_name.replace('/', '_')}_{alg_name}"
+            / f"{ds_name}_layer_{{}}_clamp_{{}}_case_{{}}.npz"
+        )
+        print(f"Will load cache from {cache_template}")
+    # Iterate through dataset
+    # Iterate through dataset
+    for record_chunks in chunks(ds, num_edits):
+        case_result_template = str(run_dir / "{}_edits-case_{}.json")
+        already_finished = True
+        for record in record_chunks:
+            if not Path(case_result_template.format(num_edits, record["case_id"])).exists():
+                already_finished = False
+                break
+        if already_finished:
+            continue
+
+        # Compute weight changes + record weights that changed
+        case_ids = [record["case_id"] for record in record_chunks]
+        args_conserve_memory = (
+            dict(return_orig_weights_device=("cpu" if conserve_memory else "cuda"))
+            if conserve_memory
+            else {}
+        )
+        etc_args = dict(cache_template=cache_template) if any(alg in alg_name for alg in ["ROME", "MEMIT"]) else {}
+
+        start = time()
+        requests = [{"case_id": record["case_id"], **record["requested_rewrite"]} for record in record_chunks]
+        filtered_requests = filter_requests(model, tok, requests)
+        print("Number of requests before filtering:", len(requests))
+        print("Number of requests after filtering:", len(filtered_requests))
+
+        # Create or append to the info.txt file with the required information
+        info_path = run_dir / "info.txt"
+        with open(info_path, "a") as file:
+            file.write(f"Model name: {model_name}\n")
+            file.write(f"Requested edits: {num_edits}\n")
+            file.write(f"Number of requests before filtering: {len(requests)}\n")
+            file.write(f"Number of requests after filtering: {len(filtered_requests)}\n")
+            file.write("Filtered\n\n")
+
+        # Get the filtered record_chunks based on filtered_requests
+        filtered_case_ids = {req['case_id'] for req in filtered_requests}
+        filtered_record_chunks = [rec for rec in record_chunks if rec['case_id'] in filtered_case_ids]
+
+        edited_model, weights_copy = apply_algo(
+            model, tok, filtered_requests, hparams, copy=False, return_orig_weights=True, **args_conserve_memory, **etc_args
+        )
+        exec_time = time() - start
+        print("Execution took", exec_time)
+
+        # Evaluate new model
+        start = time()
+        gen_test_vars = [snips, vec]
+        for record in filtered_record_chunks:
+            out_file = Path(case_result_template.format(num_edits, record["case_id"]))
+            if out_file.exists():
+                print(f"Skipping {out_file}; already exists")
+                continue
+
+            metrics = {
+                "case_id": record["case_id"],
+                "grouped_case_ids": case_ids,
+                "num_edits": num_edits,
+                "requested_rewrite": record["requested_rewrite"],
+                "time": exec_time,
+                "post": ds_eval_method(
+                    edited_model,
+                    tok,
+                    record,
+                    *(gen_test_vars if record["case_id"] % generation_test_interval == 0 else [None, None]),
+                ),
+            }
+
+            # Dump metrics in .json
+            with open(out_file, "w") as f:
+                json.dump(metrics, f, indent=1)
+
+        # Restore original weights
+        with torch.no_grad():
+            for k, v in weights_copy.items():
+                nethook.get_parameter(model, k)[...] = v.to("cuda")
+
+        print("Evaluation took", time() - start)
+
+def window(seq, n=2):
+    "Returns a sliding window (of width n) over data from the iterable"
+    "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
+    it = iter(seq)
+    result = tuple(islice(it, n))
+    if len(result) == n:
+        yield result
+    for elem in it:
+        result = result[1:] + (elem,)
+        yield result
+
+
+def chunks(arr, n):
+    """Yield successive n-sized chunks from arr."""
+    for i in range(0, len(arr), n):
+        yield arr[i : i + n]
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--alg_name",
+        choices=["MEMIT", "ROME", "FT", "MEND"],
+        default="ROME",
+        help="Editing algorithm to use. Results are saved in results/<alg_name>/<run_id>, "
+        "where a new run_id is generated on each run. "
+        "If continuing from previous run, specify the run_id in --continue_from_run.",
+        required=True,
+    )
+    parser.add_argument(
+        "--model_name",
+        choices=[
+            "gpt2-xl",
+            "EleutherAI/gpt-j-6B",
+            "EleutherAI/gpt-neox-20b",
+            "gpt2-large",
+            "gpt2-medium",
+            "gpt2",
+            "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+            "TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T",
+            "meta-llama/Llama-2-7b-hf" 
+        ],
+
+    
+        default="gpt2-xl",
+        help="Model to edit.",
+        required=True,
+    )
+    parser.add_argument(
+        "--hparams_fname",
+        type=str,
+        default="gpt2-xl.json",
+        help="Name of hyperparameters file, located in the hparams/<alg_name> folder.",
+        required=True,
+    )
+    parser.add_argument(
+        "--ds_name",
+        choices=["mcf", "cf", "zsre"],
+        default="mcf",
+        help="Dataset to perform evaluations on. Either CounterFact (cf), MultiCounterFact (mcf), or zsRE (zsre).",
+    )
+    parser.add_argument(
+        "--continue_from_run",
+        type=str,
+        default=None,
+        help="If continuing from previous run, set to run_id. Otherwise, leave as None.",
+    )
+    parser.add_argument(
+        "--dataset_size_limit",
+        type=int,
+        default=None,
+        help="Truncate CounterFact to first n records.",
+    )
+    parser.add_argument(
+        "--skip_generation_tests",
+        dest="skip_generation_tests",
+        action="store_true",
+        help="Only run fast probability-based tests without slow generation tests. "
+        "Useful for quick debugging and hyperparameter sweeps.",
+    )
+    parser.add_argument(
+        "--generation_test_interval",
+        type=int,
+        default=1,
+        help="One generation test is performed every [flag_value] iterations. If -1, generation tests are skipped.",
+    )
+    parser.add_argument(
+        "--conserve_memory",
+        dest="conserve_memory",
+        action="store_true",
+        help="Reduce memory usage during evaluation at the cost of a minor slowdown. "
+        "Backs up model weights on CPU instead of GPU.",
+    )
+    parser.add_argument(
+        "--num_edits",
+        type=int,
+        default=1,
+        help="Number of rewrites to perform simultaneously.",
+    )
+    parser.add_argument(
+        "--use_cache",
+        dest="use_cache",
+        action="store_true",
+        help="Use cached k/v pairs",
+    )
+    parser.set_defaults(skip_generation_tests=False, conserve_memory=False)
+    args = parser.parse_args()
+
+    main(
+        args.alg_name,
+        args.model_name,
+        args.hparams_fname,
+        args.ds_name,
+        args.dataset_size_limit,
+        args.continue_from_run,
+        args.skip_generation_tests,
+        args.generation_test_interval,
+        args.conserve_memory,
+        dir_name=args.alg_name,
+        num_edits=args.num_edits,
+        use_cache=args.use_cache,
+    )
